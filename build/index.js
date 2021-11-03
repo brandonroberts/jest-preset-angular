eacb555bfb1f0bf6980c1768da0fa19070e76b99
Brandon <robertsbt@gmail.com>
2021-11-03 15:43:31 -0500

    replace index transformer

--- build/index.js
+++ build/index.js
@@ -1,27 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const ts = (0, tslib_1.__importStar)(require("typescript"));
-const createTransformer = () => {
-    return {
-        canInstrument: true,
-        process(sourceText, sourcePath) {
-            const compiledOutput = ts.transpileModule(sourceText, {
-                compilerOptions: {
-                    module: ts.ModuleKind.CommonJS,
-                    target: ts.ScriptTarget.ES2015,
-                    sourceMap: true,
-                    inlineSourceMap: true,
-                    inlineSources: true,
-                },
-                fileName: sourcePath,
-            });
-            return {
-                code: compiledOutput.outputText,
-                map: compiledOutput.sourceMapText,
-            };
-        },
-    };
+const ng_jest_transformer_1 = require("./ng-jest-transformer");
+exports.default = {
+    createTransformer: () => new ng_jest_transformer_1.NgJestTransformer(),
 };
-const transformer = Object.assign(Object.assign({}, createTransformer()), { createTransformer });
-exports.default = transformer;
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,5 @@
-import { SyncTransformer, TransformOptions } from '@jest/transform';
-import * as ts from 'typescript';
+import { NgJestTransformer } from './ng-jest-transformer';
 
-type CreateTransformer = SyncTransformer<TransformOptions>['createTransformer'];
-
-const createTransformer: CreateTransformer = () => {
-  return {
-    canInstrument: true,
-    process(sourceText, sourcePath) {
-      const compiledOutput = ts.transpileModule(sourceText, {
-        compilerOptions: {
-          module: ts.ModuleKind.CommonJS,
-          target: ts.ScriptTarget.ES2015,
-          sourceMap: true,
-          inlineSourceMap: true,
-          inlineSources: true,
-        },
-        fileName: sourcePath,
-      });
-
-      return {
-        code: compiledOutput.outputText,
-        map: compiledOutput.sourceMapText,
-      };
-    },
-  };
+export default {
+  createTransformer: (): NgJestTransformer => new NgJestTransformer(),
 };
-
-const transformer: SyncTransformer<TransformOptions> = {
-  ...createTransformer(),
-  // Assigned here so only the exported transformer has `createTransformer`,
-  // instead of all created transformers by the function
-  createTransformer,
-};
-
-export default transformer;
